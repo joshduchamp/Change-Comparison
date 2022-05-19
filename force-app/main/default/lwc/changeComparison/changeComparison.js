4d06f974e7b2f73c13c17beee3b1d25f28d8b95f
Joshua Duchamp <joshduchamp@gmail.com>
2022-05-18 23:11:36 -0500

    Updated component to use metadata

--- force-app/main/default/lwc/changeComparison/changeComparison.js
+++ force-app/main/default/lwc/changeComparison/changeComparison.js
@@ -1,18 +1,30 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+const METADATA_FIELDS = [
+    'Change_Comparison_Mapping__mdt.Field_Labels__c',
+    'Change_Comparison_Mapping__mdt.Hide_Matching_Values__c',
+    'Change_Comparison_Mapping__mdt.Root_Record_Api_Name__c',
+    'Change_Comparison_Mapping__mdt.Title__c',
+    'Change_Comparison_Mapping__mdt.ValueA_Api_Name__c',
+    'Change_Comparison_Mapping__mdt.ValueA_Fields__c',
+    'Change_Comparison_Mapping__mdt.ValueA_Header__c',
+    'Change_Comparison_Mapping__mdt.ValueA_Id_Field__c',
+    'Change_Comparison_Mapping__mdt.ValueB_Api_Name__c',
+    'Change_Comparison_Mapping__mdt.ValueB_Fields__c',
+    'Change_Comparison_Mapping__mdt.ValueB_Header__c',
+    'Change_Comparison_Mapping__mdt.ValueB_Id_Field__c',
+];
 
 export default class ChangeComparison extends LightningElement {
-    @api title = ''
-    @api headerA = 'Old Value'
-    @api headerB = 'New Value'
     @api recordId;
-    @api recordIdAField;
-    @api recordIdBField;
-    @api fieldLabelsDelimited;
-    @api fieldListADelimited;
-    @api fieldListBDelimited;
-    @api hideMatchingValues = false;
-    @track fieldLabels;
+    @api metadataId;
+    @track title = ''
+    @track headerA = 'Old Value'
+    @track headerB = 'New Value'
+    @track recordIdAField;
+    @track recordIdBField;
+    @track hideMatchingValues = false;
+    @track fieldLabels = [];
     @track fieldListA = [];
     @track fieldListB = [];
     @track rootIdFieldList = [];
@@ -28,10 +40,28 @@ export default class ChangeComparison extends LightningElement {
     }
 
     connectedCallback() {
-        this.fieldLabels = this.fieldLabelsDelimited ? this.fieldLabelsDelimited.split(',') : [];
-        this.fieldListA = this.fieldListADelimited ? this.fieldListADelimited.split(',') : [];
-        this.fieldListB = this.fieldListBDelimited ? this.fieldListBDelimited.split(',') : [];
-        this.rootIdFieldList = [this.recordIdAField, this.recordIdBField];
+        console.log('recordId: ' + this.recordId);
+        console.log('metadataId: ' + this.metadataId);
+    }
+
+    @wire(getRecord, { recordId: '$metadataId', fields: METADATA_FIELDS })
+    getMetadata({data, error}) {
+        if (data) {
+            console.log(data);
+            this.fieldLabels = data.fields.Field_Labels__c.value.split('\n').map(x => x.trim());
+            this.hideMatchingValues = data.fields.Hide_Matching_Values__c.value;
+            this.recordIdAField = data.fields.Root_Record_Api_Name__c.value + '.' + data.fields.ValueA_Id_Field__c.value;
+            this.recordIdBField = data.fields.Root_Record_Api_Name__c.value + '.' + data.fields.ValueB_Id_Field__c.value;
+            this.rootIdFieldList = [this.recordIdAField, this.recordIdBField];
+            this.title = data.fields.Title__c.value;
+            this.fieldListA = data.fields.ValueA_Fields__c.value.split('\n').map(x => data.fields.ValueA_Api_Name__c.value + '.' + x.trim());
+            this.fieldListB = data.fields.ValueB_Fields__c.value.split('\n').map(x => data.fields.ValueB_Api_Name__c.value + '.' + x.trim());
+            this.headerA = data.fields.ValueA_Header__c.value;
+            this.headerB = data.fields.ValueB_Header__c.value;
+        }
+        if (error) {
+            console.log('error: ' + JSON.stringify(error));
+        }
     }
 
     @wire(getRecord, { recordId: '$recordId', fields: '$rootIdFieldList' })
